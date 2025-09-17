887e6cb6988b5bb17c586046292091e550da6e6b
cpedrero <cpedrero@protonmail.com>
2025-09-17 03:03:04 +0200

    working in cloud run with 2GB memory

--- src/main/java/org/example/PdfConverter.java
+++ src/main/java/org/example/PdfConverter.java
@@ -1,36 +1,50 @@
 package org.example;
 
-import com.google.cloud.functions.BackgroundFunction;
-import com.google.cloud.functions.Context;
+import com.google.cloud.functions.CloudEventsFunction;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import io.cloudevents.CloudEvent;
 import org.docx4j.Docx4J;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class PdfConverter implements BackgroundFunction<PdfConverter.GCSEvent> {
+public class PdfConverter implements CloudEventsFunction {
 
   private static final Gson gson = new Gson();
 
   @Override
-  public void accept(GCSEvent event, Context context) {
-    System.out.println("Bucket: " + event.bucket);
-    System.out.println("File: " + event.name);
-    convert(event.bucket, event.name);
-  }
+  public void accept(CloudEvent event) throws Exception {
+    logger.info("Event Type: " + event.getType());
+    logger.info("Event Subject: " + event.getSubject());
 
-  public static class GCSEvent {
-    String bucket;
-    String name;
-    String contentType;
-    String timeCreated;
-    String updated;
+    String data = new String(event.getData().toBytes(), StandardCharsets.UTF_8);
+    final var storageData = gson.fromJson(data, JsonObject.class);
+
+    logger.info("protoPayload: " + storageData.get("protoPayload").getAsJsonObject());
+
+    final String resourceName = storageData.getAsJsonObject("protoPayload").get("resourceName").getAsString();
+
+    Pattern pattern = Pattern.compile("buckets/([^/]+)/objects/(.+)");
+    Matcher matcher = pattern.matcher(resourceName);
+    if (matcher.find()) {
+      String bucket = matcher.group(1);
+      String object = matcher.group(2);
+
+      logger.info("bucket: " + bucket);
+      logger.info("Objeto: " + object);
+      convert(bucket, object);
+
+    }
   }
 
   private static final Logger logger = Logger.getLogger(PdfConverter.class.getName());
@@ -46,7 +60,7 @@ public class PdfConverter implements BackgroundFunction<PdfConverter.GCSEvent> {
 
       Docx4J.toPDF(wordMLPackage, pdfOutput);
 
-      uploadFile(bucketName, objectName+".pdf", pdfOutput.toByteArray());
+      uploadFile(bucketName, objectName + ".pdf", pdfOutput.toByteArray());
       logger.info("PDF generado con docx4j");
 
     } catch (Docx4JException | IOException e) {
